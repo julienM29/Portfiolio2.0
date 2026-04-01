f3b4ebdbec9e9c4af2b34769eb5c1978f919b883
julienM29 <137764146+julienM29@users.noreply.github.com>
2026-04-01 15:46:44 +0200

    Modifications css img

--- assets/dashboardAccueilDark-CWEUxjnN.js
+++
@@ -1,1 +0,0 @@
-const a=""+new URL("dashboardAccueilDark-D6oCh_3B.webp",import.meta.url).href;export{a as i};
--- assets/site_kerisnel-Dxh1FZLe.js
+++
@@ -1,1 +0,0 @@
-const e=""+new URL("site_kerisnel-BtJfupUJ.webp",import.meta.url).href;export{e as l};
